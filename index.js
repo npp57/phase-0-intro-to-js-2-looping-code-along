ce248e3409ca035b204d434f26491a7a35cfd1b9
Neal Patel <xnealx66@gmail.com>
2022-06-15 14:27:43 -0400

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+let cards = ['Guadalupe', 'Ollie', 'Aki'];
+  function writeCards(cards) {
+    let array = [];
+    for (let i = 0; i < cards.length; i++) {
+    array.push(`Thank you, ${cards[i]}, for the wonderful surprise gift!`);
+   debugger; 
+  } 
+   return array;
+}
+
+
+function countDown(startNumber){
+        while (startNumber >= 0) {
+        console.log(startNumber);
+        startNumber --;
+    
+    }
+}
+
+countDown(10)
